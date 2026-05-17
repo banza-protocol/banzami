7b8482c6b23b88b69f1209a2e7cb3fcffd04e732
Banzami <contact@banzami.org>
2026-05-17 13:00:17 +0200

    fix(splash): reduce logo to 80pt on both native and Flutter splash screens

--- apps/mobile/lib/screens/splash_screen.dart
+++ apps/mobile/lib/screens/splash_screen.dart
@@ -47,7 +47,7 @@ class _Logo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Image.asset(
       'assets/images/banzami_splash.png',
-      width: 100,
+      width: 80,
     );
   }
 }
